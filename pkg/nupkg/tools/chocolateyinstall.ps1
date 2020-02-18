958877af7c804d8d9685bea788d0779610e4ac8e
Clemens Wacha <clemens.wacha@ubs.com>
2020-02-18 16:45:20 +0100

    fixed links to code.bat

--- pkg/nupkg/tools/chocolateyinstall.ps1
+++ pkg/nupkg/tools/chocolateyinstall.ps1
@@ -1,5 +1,7 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #Install-BinFile -Name npp-menu -Path $toolsDir\root\install_context_menu_open_with_notepad.cmd
-if !(Test-Path $toolsDir\root\data\user-data\User\settings.json -PathType Leaf) {
+if (!(Test-Path $toolsDir\root\data\user-data\User\settings.json -PathType Leaf)) {
 	Copy-Item $toolsDir\root\data\user-data\User\settings.json.dist $toolsDir\root\data\user-data\User\settings.json
 }
+
+Install-BinFile -Name code -Path $toolsDir\root\bin\code.cmd
