c59c5643edd14c5000e20a1573c69da985a0e6d1
Clemens Wacha <clemens.wacha@ubs.com>
2020-02-19 16:36:16 +0100

    added right-click menus and start menu entry

--- pkg/nupkg/tools/chocolateyinstall.ps1
+++ pkg/nupkg/tools/chocolateyinstall.ps1
@@ -4,4 +4,11 @@ if (!(Test-Path $toolsDir\root\data\user-data\User\settings.json -PathType Leaf)
 	Copy-Item $toolsDir\root\data\user-data\User\settings.json.dist $toolsDir\root\data\user-data\User\settings.json
 }
 
-Install-BinFile -Name code -Path $toolsDir\root\bin\code.cmd
+& $toolsDir\root\install_context_menu_open_with_vscode.cmd
+Install-BinFile -Name code-menu -Path $toolsDir\root\install_context_menu_open_with_vscode.cmd
+
+$startmenudir = "$env:AppData\Microsoft\Windows\Start Menu\Programs"
+"Creating Start Menu entry"
+Install-ChocolateyShortcut `
+    -ShortcutFilePath "$startmenudir\VSCode.lnk"`
+    -TargetPath "$toolsDir\root\Code.exe"
---
+++ src/install_context_menu_open_with_vscode.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+set BASEDIR=%~dp0
+if "%BASEDIR:~-1%" == "\" set BASEDIR=%BASEDIR:~0,-1%
+
+set action=Edit with VSCode
+set command=%BASEDIR%\Code.exe
+
+
+:: Detect if we have Administrator rights
+net session >nul 2>&1
+if %errorlevel% == 0 (
+	set hkey=HKEY_LOCAL_MACHINE
+) else (
+	set hkey=HKEY_CURRENT_USER
+)
+
+:: File right click
+reg.exe add "%hkey%\Software\Classes\*\shell\%action%\command" /ve /t REG_EXPAND_SZ /d "\"%command%\"\"%%1\"" /f
+reg.exe add "%hkey%\Software\Classes\*\shell\%action%" /v Icon /t REG_SZ /d "%command%,0" /f
+
+
+:: Folder right click
+reg.exe add "%hkey%\Software\Classes\Directory\shell\%action%\command" /ve /t REG_EXPAND_SZ /d "\"%command%\"\"%%1\"" /f
+reg.exe add "%hkey%\Software\Classes\Directory\shell\%action%" /v Icon /t REG_SZ /d "%command%,0" /f
+
+
+:: Explorer Background right click
+reg.exe add "%hkey%\Software\Classes\Directory\Background\shell\%action%\command" /ve /t REG_EXPAND_SZ /d "\"%command%\"\"%%1\"" /f
+reg.exe add "%hkey%\Software\Classes\Directory\Background\shell\%action%" /v Icon /t REG_SZ /d "%command%,0" /f
